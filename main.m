f11236b6533468f054b0063d917537a605b4bbab
Jerrypoi <jerry.poi.sz@gmail.com>
2019-03-21 21:33:20 +0800

    Add notes on findHighFreq

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
 
 % Load data
-load('./data/1.mat')
+load('./data/4.mat')
 % Cut data
 cut
 
 for i = 1 : size(ssbeg')
 %    display(distinguish(data(ssbeg(i):ssend(i))));
-    display(findHighFreq(data(ssbeg(i):ssend(i)),fs,1600) - findLowFreq(data(ssbeg(i):ssend(i)),fs));
+    % The second argument for findHighFreq is the desinated frequency.
+    % I tried from 1200 to 3200.
+    display(findHighFreq(data(ssbeg(i):ssend(i)),fs,3200) - findLowFreq(data(ssbeg(i):ssend(i)),fs));
 end
